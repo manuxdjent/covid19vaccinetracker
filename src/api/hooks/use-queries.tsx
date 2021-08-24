cf00de27178d6e1a6d942a5208075369247da8e9
Manux Djent <manuxdjent@gmail.com>
2021-08-24 20:57:54 +0200

    fix const naming

--- src/api/hooks/use-queries.tsx
+++ src/api/hooks/use-queries.tsx
@@ -2,14 +2,14 @@ import { useState, useEffect } from 'react'
 import { Vaccines, Phase } from '../../models/common.model'
 import { get } from '../fetchers/fetchers'
 
-const MIDDLEWARE_URI = process.env.NODE_ENV === 'production' ? 'https://vaccinetracker-middleware.herokuapp.com' : 'http://localhost:3000'
+const MIDDLEWARE_URL = process.env.NODE_ENV === 'production' ? 'https://vaccinetracker-middleware2.herokuapp.com' : 'http://localhost:3000'
 
 export const useGetVaccineCandidates = () => {
     const [vaccineCandidates, setCandidates] = useState<Vaccines>()
     const [loading, setLoading] = useState<boolean>(true)
 
     const getCandidates = async () => {
-        const result = await get<Vaccines>(`${MIDDLEWARE_URI}/data`)
+        const result = await get<Vaccines>(`${MIDDLEWARE_URL}/data`)
         setCandidates(result)
         setLoading(false)
     }
@@ -26,7 +26,7 @@ export const useGetPhases = () => {
     const [loading, setLoading] = useState<boolean>(true)
 
     const getPhases = async () => {
-        const result = await get<[Phase]>(`${MIDDLEWARE_URI}/phases`)
+        const result = await get<[Phase]>(`${MIDDLEWARE_URL}/phases`)
         setPhases(result)
         setLoading(false)
     }
